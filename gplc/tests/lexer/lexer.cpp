c54daeb31982fad9e389437ad68992bf3f1c5865
jekyll0_0 <ildar2571@yandex.ru>
2016-07-14 20:47:36 +0400

    Date:		14.07.2016 Author:		Kasimov Ildar Description:	Unit tests for CLexer class were added into the project. All of them are successfully passed.

--- gplc/tests/lexer/lexer.cpp
+++ gplc/tests/lexer/lexer.cpp
@@ -19,9 +19,44 @@ TEST_CASE("Lexer's tests")
 		REQUIRE(pIdentifierToken->GetName() == L"identifier");
 	}
 
+	SECTION("Correct identifier with digits test")
+	{
+		REQUIRE(pLexer->Init(L"identifier42 identi42fier") == gplc::RV_SUCCESS);
+
+		const gplc::CIdentifierToken* pIdentifierToken = dynamic_cast<const gplc::CIdentifierToken*>(pLexer->GetCurrToken());
+
+		REQUIRE(pIdentifierToken->GetType() == gplc::TT_IDENTIFIER);
+		REQUIRE(pIdentifierToken->GetName() == L"identifier42");
+
+		pIdentifierToken = dynamic_cast<const gplc::CIdentifierToken*>(pLexer->GetNextToken());
+
+		REQUIRE(pIdentifierToken->GetType() == gplc::TT_IDENTIFIER);
+		REQUIRE(pIdentifierToken->GetName() == L"identi42fier");
+	}
+
+	SECTION("Correct identifier with underscope test")
+	{
+		REQUIRE(pLexer->Init(L"_identifier compound_identifier_test id_") == gplc::RV_SUCCESS);
+
+		const gplc::CIdentifierToken* pIdentifierToken = dynamic_cast<const gplc::CIdentifierToken*>(pLexer->GetCurrToken());
+
+		REQUIRE(pIdentifierToken->GetType() == gplc::TT_IDENTIFIER);
+		REQUIRE(pIdentifierToken->GetName() == L"_identifier");
+
+		pIdentifierToken = dynamic_cast<const gplc::CIdentifierToken*>(pLexer->GetNextToken());
+
+		REQUIRE(pIdentifierToken->GetType() == gplc::TT_IDENTIFIER);
+		REQUIRE(pIdentifierToken->GetName() == L"compound_identifier_test");
+
+		pIdentifierToken = dynamic_cast<const gplc::CIdentifierToken*>(pLexer->GetNextToken());
+
+		REQUIRE(pIdentifierToken->GetType() == gplc::TT_IDENTIFIER);
+		REQUIRE(pIdentifierToken->GetName() == L"id_");
+	}
+
 	SECTION("Invalid \'digit at the begining of the identifier\' identifier test")
 	{
-		REQUIRE(pLexer->Init(L"9identifier") == gplc::RV_FAIL);
+		REQUIRE(pLexer->Init(L"42identifier") == gplc::RV_FAIL);
 	}
 
 	delete pLexer;
